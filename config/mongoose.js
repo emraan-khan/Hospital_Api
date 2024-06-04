655e3f1f6f2667201b4357db94a0ad3638e27598
Imran Khan <imrankhan772244@gmail.com>
2024-06-04 23:19:31 +0530

    mongoose update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', true);
+mongoose.set('useCreateIndex', true);
+
+// Database connection 
+mongoose.connect('mongodb://127.1.1.1:2707/hospital_API' , { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+// connection Error handling
+db.on('error', console.error.bind(console, "Error connection to the database"));
+
+// Connection Error Successful
+db.once('open' , function(){
+    console.log('Successfully connected to the Database');
+})
