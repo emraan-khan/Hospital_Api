80f1252603faffeb1309224cf723cf2a5dda54a8
Imran Khan <imrankhan772244@gmail.com>
2024-06-07 19:39:19 +0530

    doctor controller done

--- controllers/api/v1/doctor_controller.js
+++ controllers/api/v1/doctor_controller.js
@@ -1,2 +1,72 @@
 // Importing Dependencies
 const Doctor = require('../../../models/doctorModel');
+const jwt = require('jsonwebtoken');
+
+// Doctor Registration
+module.exports.register = async function(req,res){
+    try{
+        const doctor= await Doctor.create(req.body);
+        return res.status(200).json({
+            // if doctor registration is successfull
+            success: true,
+            message: doctor
+        })
+    }catch (err){
+
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        });
+
+    }
+}
+
+// Doctor Sign in
+module.exports.login = async(req, res)=>{
+    try{
+        let {email , password} = req.body;
+
+        // if email or password not exist
+        if(!email || !password){ 
+            res.status(400).json({
+                success: false,
+                message: 'No Email or Password'
+            })
+        }
+
+        let doctor = await Doctor.findOne({email})
+        // if doctor does not exist
+        if(!doctor){
+            res.status(401).json({
+                success: false,
+                message: 'Invalid Username or Password!'
+            })
+        }
+
+        // check if password matches
+        const isMatch = await doctor.matchPassword(password);
+        // if password is wrong
+        if(!isMatch){
+            res.status(401).json({
+                success: false,
+                message: 'Invalid Username or Password!'
+            })
+        }
+
+        // getting the jwt token
+        const token = doctor.getSignedJwtToken();
+
+        // return respoonse
+        res.status(200).json({
+            success: true,
+            token,
+            message: `Logged In successfully! Keep the Token Safe ${doctor.username}`        })
+
+    } catch(err){
+        console.log(err);
+        res.status(400).json({
+            success: false,
+            message: 'Error Occurred!'
+        })
+    }
+}
--- models/reportModel.js
+++ models/reportModel.js
@@ -6,5 +6,25 @@ const reportSchema = new mongoose.Schema({
     doctor: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Doctor'
+    },
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient'
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Negative', 'Travelled-Quarantine', 'Symptoms-Quarantine', 'Positive-Admit']
+    },
+    data: {
+        type: Date,
+        default: Date.now,
+        required: true,
     }
-})
+}, {
+    timestamps: true
+});
+
+// Exports
+const Report = mongoose.model('Report', reportSchema);
+module.exports = Report;
