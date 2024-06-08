5a77526d61519a98c7c2fc748c8e191ddc8f5a30
Imran Khan <imrankhan772244@gmail.com>
2024-06-08 20:16:08 +0530

    verify token

--- controllers/api/v1/report_controller.js
+++ controllers/api/v1/report_controller.js
@@ -3,14 +3,14 @@ const Doctor = require('../../../models/doctorModel');
 const Patient = require('../../../models/patientModel');
 const Report = require('../../../models/reportModel');
 
-module.exports.create_report = async function(req, res){
+module.exports.create_report = async function (req, res) {
     console.log('Inside report controller');
 
     // getting the doctor id
 
     const doctor = req.doctor._id;
-    console.log('Dr: '+doctor);
-    try{
+    console.log('Dr: ' + doctor);
+    try {
         console.log("Inside try");
         // Creating Report
         const report = await Report.create({
@@ -19,7 +19,51 @@ module.exports.create_report = async function(req, res){
             status: req.body.status
         })
 
-    }catch(err){
+        return res.status(200).json({
+            success: true
+        })
+    } catch (err) {
 
+        // erro handling in the catch block
+        return res.status(401).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+// send all reports
+module.exports.all_reports = async function (req, res) {
+    try {
+        const reports = Report.find({
+            "patient": req.params.id
+        })
+        reports.exec(function (err, report) {
+            return res.send(report);
+        })
+    } catch (err) {
+        // erro handling in the catch block
+        return res.status(401).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+
+// sending reports by status
+module.exports.report_by_status = async (req, res) => {
+    try {
+        
+        const reports = Report.find({
+            'status': req.params.status
+        })
+        reports.exec(function (err, req) {
+            return res.send(rep);
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
     }
 }
